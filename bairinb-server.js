29b106d3211b24acb8f722e9ef20e03f4a0c9da5
Pedro Antonio <petterlx727@gmail.com>
2016-02-13 00:37:29 -0600

    Sessions were supported

--- bairinb-server.js
+++ bairinb-server.js
@@ -3,7 +3,12 @@ var swig = require('swig')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var uuid = require('uuid')
+var bcrypt = require('bcrypt-nodejs')
 var Schema = mongoose.Schema
+
+var session = require('express-session')
+var MongoStore = require('express-session-mongo')
+var flash = require('flash')
 
 var app = express()
 
@@ -11,11 +16,25 @@ app.engine('html', swig.renderFile)
 app.set('view engine', 'html')
 app.set('views', __dirname + '/views')
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(session({
+	secret: 'keyboard cat',
+	store: new MongoStore(),
+	saveUninitialized: true,
+	resave: true
+}))
 
+app.use( flash() )
 app.set('view_cache', false)
 swig.setDefaults({cache : false})
 
 mongoose.connect('mongodb://localhost/bairinb')
+
+var userSchema = Schema({
+	username: String,
+	displayName: String,
+	password: String,
+	uuid : {type: String, default: uuid.v4}
+})
 
 var city = new Schema({
   uuid: {type:String, default: uuid.v4},
@@ -31,8 +50,24 @@ var advert = new Schema({
   city_uuid: String
 })
 
+var User = mongoose.model('User', userSchema)
 var City = mongoose.model('City', city)
 var Advert = mongoose.model('Advert', advert)
+
+app.use(function (req, res, next) {
+	if(!req.session.userId){
+		return next()
+	}
+
+	User.findOne({uuid: req.session.userId}, function(err, user){
+		if(err){
+			return res.send(500, 'Internal Server Error')
+		}
+
+		res.locals.user = user
+		next()
+	})
+});
 
 app.get('/', function(request, response){
 
@@ -44,6 +79,90 @@ app.get('/', function(request, response){
     response.render('index', {title : 'Bairinb', cities : docs})
 
   })
+})
+
+app.get('/sign-up', function (req, res){
+	var error = res.locals.flash.pop()
+
+	res.render('sign-up', {
+		error: error
+	})
+})
+
+app.get('/log-in', function (req, res){
+	var error = res.locals.flash.pop()
+
+	res.render('log-in',{
+		error: error
+	})
+})
+
+app.get('/log-out', function (req, res){
+	req.session.destroy()
+	res.redirect('/')
+})
+
+app.post('/sign-up', function (req, res){
+	if(!req.body.username || !req.body.password){
+		req.flash('sign-up-error', 'To sign up you need a username and a password')
+		return res.redirect('/sign-up')
+	}
+
+	User.findOne({username: req.body.username}, function(err, doc){
+		if(err){
+			return res.send(500, 'Internal Server Error')
+		}
+
+		if(doc){
+			req.flash('sign-up-error', 'Username is taken')
+			return res.redirect('/sign-up')
+		}
+
+		bcrypt.hash(req.body.password, null/* Salt */, null, function(err, hashedPassword) {
+			if(err){
+				return res.send(500, 'Internal Server Error')
+			}
+
+			User.create({
+				username: req.body.username,
+				password: hashedPassword
+			}, function(err, doc){
+				if(err){
+					return res.send(500, 'Internal Server Error')
+				}
+
+				req.session.userId = doc.uuid
+				res.redirect('/')
+			})
+		});
+	})
+})
+
+app.post('/log-in', function (req, res){
+	if(!req.body.username || !req.body.password){
+		req.flash('log-in-error', 'To log in you need a username and a password')
+		return res.redirect('/log-in')
+	}
+
+	User.findOne({username: req.body.username}, function(err, doc){
+		if(err){
+			return res.send(500, 'Internal Server Error')
+		}
+
+		if(!doc){
+			req.flash('log-in-error', 'Invalid user')
+			return res.redirect('/log-in')
+		}
+
+		bcrypt.compare(req.body.password, doc.password, function(err, result){
+			if(err){
+				return res.send(500, 'Internal Server Error')
+			}
+
+			req.session.userId = doc.uuid
+			res.redirect('/')
+		})
+	})
 })
 
 app.get('/add-listing', function(request, response){
